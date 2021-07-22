896f359bca84e0f73623dff21968134f5dc5e31d
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-07-22 04:50:39 +0000

    fix 100, travel to up

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,13 +7,21 @@
  * Return: lowest common ancestor node of the two given nodes
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-				const binary_tree_t *second);
+				const binary_tree_t *second)
 {
 	if (!first || !second)
 		return (0);
 
 	if (first == second)
-		return (first);
+		return ((binary_tree_t *) first);
+
+	if (first == second->parent || !first->parent ||
+	(!first->parent && second->parent))
+		return (binary_trees_ancestor(first, second->parent));
+
+	if (second == first->parent || !second->parent ||
+	(!second->parent && first->parent))
+		return (binary_trees_ancestor(first->parent, second));
 
 	return (binary_trees_ancestor(first->parent, second->parent));
 }
