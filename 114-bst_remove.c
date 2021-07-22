a98fd23fc60f15714d20532baf27ec2c10aecba3
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-07-22 21:51:25 +0000

    fix 114 115

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,5 +1,48 @@
 #include "binary_trees.h"
 
+bst_t *bst_remove_node(bst_t *, bst_t *);
+
+/**
+ * bst_remove_node - removes a node from a Binary Search Tree
+ * @root: root of the tree
+ * @cur: pointer to the node will remove
+ * Return: pointer to the new root node of the tree after removing
+ */
+bst_t *bst_remove_node(bst_t *root, bst_t *cur)
+{
+	bst_t *new_root = cur->right ? cur->right : cur->left;
+
+	while (new_root && new_root->left)
+		new_root = new_root->left;
+
+	if (new_root)
+	{
+		new_root->left = cur->left;
+
+		if (cur->left)
+			cur->left->parent = new_root;
+		new_root->right = cur->right;
+
+		if (cur->right)
+			cur->right->parent = new_root;
+
+		new_root->parent->left = 0;
+		new_root->parent = cur->parent;
+
+		if (!new_root->parent)
+			root = new_root;
+	}
+	if (cur->parent)
+	{
+		if (cur->parent->left == cur)
+			cur->parent->left = new_root;
+		else
+			cur->parent->right = new_root;
+	}
+	free(cur);
+	return (root);
+}
+
 /**
  * bst_remove - removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree where you will remove a node
@@ -8,42 +51,16 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *cur = root, *new_root = 0;
+	bst_t *cur = root;
 
 	while (cur)
 	{
 		if (cur->n == value)
-		{
-			new_root = cur->right ? cur->right : cur->left;
+			return (bst_remove_node(root, cur));
 
-			while (new_root && new_root->left)
-				new_root = new_root->left;
-			new_root->parent->left = 0;
-			new_root->parent = cur->parent;
-
-			if (cur->parent)
-			{
-				if (cur->parent->left == cur)
-					cur->parent->left = new_root;
-				else
-					cur->parent->right = new_root;
-			}
-			new_root->left = cur->left;
-
-			if (cur->left)
-				cur->left->parent = new_root;
-			new_root->right = cur->right;
-
-			if (cur->right)
-				cur->right->parent = new_root;
-			free(cur);
-
-			if (!new_root->parent)
-				root = new_root;
-			return (root);
-		}
 		else if (cur->n > value)
 			cur = cur->left;
+
 		else
 			cur = cur->right;
 	}
